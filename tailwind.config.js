96d6fb0750dd99d538a156cb8991cb16cb31a2d3
Hoa0304 <tranthicamhoa2k4@gmail.com>
2024-09-22 14:35:59 +0700

    #1: color definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,22 @@ export default {
         italianno: ['Italianno', 'cursive'],
         inter: ['Inter', 'sans-serif'],
         poppins: ['Poppins','sans-serif'],
-      }
+      },
+      colors: {
+        primary: '#FFFFFF',
+        secondary: '#C10C99',
+        tertiary: '#B5B5B5',
+        quaternary: '#CD46D9',
+        gradientStart: '#CD46D9',
+        gradientEnd: '#27C5C9',
+        bg: '#2F3133',
+      },
+      fontSize: {
+        'tiny': '13px',
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
   ],
 }
 
