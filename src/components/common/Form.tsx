91b655b4616208e37b41dac61e40e7aa26a432a2
Hoa0304 <tranthicamhoa2k4@gmail.com>
2024-10-10 21:30:24 +0700

    #14, #8 update form and sidebar (#6)

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,70 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import InputField from './InputField';
 import SelectField from './SelectField';
-import { branches } from '../../constants';
-import { Product } from '../../types/product.types';
-import axios from 'axios';
-interface FormProps {
-    productToEdit?: Product;
-    onFormSubmit: (product: Product) => void;
-}
+import { branches, stars } from '../../constants';
+import { FormProps } from '../../types/product.types';
+import useProductSate from '../../hooks/product/useProductState';
+import { addProduct, editProduct } from '../../services/productService';
 
 export const Form: React.FC<FormProps> = ({ productToEdit, onFormSubmit }) => {
-    const [branch, setBranch] = useState('Specials');
-    const [amount, setAmount] = useState('');
-    const [price, setPrice] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
-
-    const [formData, setFormData] = useState<Product>({
-        image: '',
-        name: '',
-        amount: 0,
-        price: 0,
-        branch
-    });
-
-    useEffect(() => {
-        if (productToEdit) {
-            setFormData(productToEdit);
-            setBranch(productToEdit.branch);
-            setAmount(String(productToEdit.amount));
-            setPrice(String(productToEdit.price));
-            setImageUrl(productToEdit.image);
-        }
-    }, [productToEdit]);
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { id, value } = e.target;
-        setFormData(prev => ({ ...prev, [id]: value }));
-    };
-
-    const handleBranchChange = (value: string) => {
-        setBranch(value);
-        setFormData(prev => ({ ...prev, branch: value }));
-    };
-
-    const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        const numericValue = Number(value);
-        if (numericValue >= 1) {
-            setAmount(value);
-            setFormData(prev => ({ ...prev, amount: numericValue }));
-        } else {
-            setAmount('');
-        }
-    };
-
-    const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setPrice(value);
-        setFormData(prev => ({ ...prev, price: Number(value) }));
-    };
-
-    const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setImageUrl(value);
-        setFormData(prev => ({ ...prev, image: value }));
-    };
+    const {
+        formData,
+        branch,
+        amount,
+        price,
+        imageUrl,
+        star,
+        handleInputChange,
+        handleBranchChange,
+        handleAmountChange,
+        handlePriceChange,
+        handleUrlChange,
+        handleStarChange,
+        resetForm
+    } = useProductSate(productToEdit || null);
 
     const handleAddOrEdit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -73,92 +30,96 @@ export const Form: React.FC<FormProps> = ({ productToEdit, onFormSubmit }) => {
             branch,
             amount: Number(amount),
             price: Number(price),
-            image: imageUrl
+            image: imageUrl,
+            star: Number(star)
         };
 
         try {
-            if (productToEdit) {
-                await axios.put(`http://localhost:8000/products/${productToEdit.id}`, productData);
+            if (productToEdit && productToEdit.id !== undefined) {
+                await editProduct(productToEdit.id, productData);
             } else {
-                await axios.post('http://localhost:8000/products', productData);
+                await addProduct(productData);
             }
-
             onFormSubmit(productData);
-
-            setFormData({ image: '', name: '', amount: 0, price: 0, branch: 'Specials' });
-            setBranch('Specials');
-            setAmount('');
-            setPrice('');
-            setImageUrl('');
+            resetForm();
         } catch (error) {
             console.error('Error adding or updating product:', error);
         }
     };
 
     return (
-            <form
-                className="flex flex-col gap-3 bg-bg w-full h-auto p-9 rounded-3xl border-2 border-secondary"
-                onSubmit={handleAddOrEdit}
-            >
-                <div className="flex gap-4">
-                    <div className="flex-1 mt-5">
-                        <InputField
-                            type="text"
-                            placeholder="Paste image URL here"
-                            value={imageUrl}
-                            onChange={handleUrlChange}
-                            id='image'
-                            label='Image URL'
-                            classNamePrefix='mb-5'
-                        />
-                        <InputField
-                            type="text"
-                            placeholder="Enter your Name"
-                            id="name"
-                            label="Name"
-                            value={formData.name}
-                            onChange={handleInputChange}
-                            classNamePrefix='mb-5'
-                        />
-                        <SelectField
-                            id="branch"
-                            label="Branch"
-                            placeholder="Select Branch"
-                            value={branch}
-                            onChanges={handleBranchChange}
-                            options={branches}
-                        />
-                    </div>
-                    {imageUrl && (
-                        <img
-                            src={imageUrl}
-                            alt="Preview"
-                            className="w-1/2 h-auto rounded-lg"
-                        />
-                    )}
+        <form
+            className="flex flex-col gap-3 bg-bg w-full h-auto p-9 rounded-3xl border-2 border-secondary"
+            onSubmit={handleAddOrEdit}
+        >
+            <div className="flex gap-2">
+                <div className="flex-1 mt-4">
+                    <InputField
+                        type="text"
+                        placeholder="Paste image URL here"
+                        value={imageUrl}
+                        onChange={(e) => handleUrlChange(e.target.value)}
+                        id='image'
+                        label='Image URL'
+                        classNamePrefix='mb-2'
+                    />
+                    <InputField
+                        type="text"
+                        placeholder="Enter your Name"
+                        id="name"
+                        label="Name"
+                        value={formData.name}
+                        onChange={(e) => handleInputChange('name', e.target.value)}
+                        classNamePrefix='mb-2'
+                    />
+                    <SelectField
+                        id="branch"
+                        label="Branch"
+                        placeholder="Select Branch"
+                        value={branch}
+                        onChanges={handleBranchChange}
+                        options={branches}
+                        classNamePrefix='mb-7'
+                    />
+                    <SelectField
+                        id="star"
+                        label="Star Rating"
+                        placeholder="Select Star Rating"
+                        value={String(star)}
+                        onChanges={(value) => handleStarChange(Number(value))}
+                        options={stars.map(starValue => String(starValue))}
+                    />
                 </div>
+                {imageUrl && (
+                    <img
+                        src={imageUrl}
+                        alt="Preview"
+                        className="w-1/2 h-[100%] rounded-lg"
+                    />
+                )}
+            </div>
 
-                <InputField
-                    type="number"
-                    placeholder="Enter amount"
-                    id="amount"
-                    label="Amount"
-                    value={amount}
-                    onChange={handleAmountChange}
-                />
-                <InputField
-                    type="number"
-                    placeholder="Enter price"
-                    id="price"
-                    label="Price"
-                    value={price}
-                    onChange={handlePriceChange}
-                    isPriceField={true}
-                />
+            <InputField
+                type="number"
+                placeholder="Enter amount"
+                id="amount"
+                label="Amount"
+                value={amount}
+                onChange={(e) => handleAmountChange(e.target.value)}
+            />
+            <InputField
+                type="number"
+                placeholder="Enter price"
+                id="price"
+                label="Price"
+                value={price}
+                onChange={(e) => handlePriceChange(e.target.value)}
+                isPriceField={true}
+            />
 
-                <button type="submit" className="mt-2 bg-secondary text-white py-2 rounded-xl">
-                    {productToEdit ? 'Update' : 'Create'}
-                </button>
-            </form>
+            <button type="submit" className="mt-2 bg-secondary text-white py-2 rounded-xl">
+                {productToEdit ? 'Update' : 'Create'}
+            </button>
+        </form>
     );
 };
